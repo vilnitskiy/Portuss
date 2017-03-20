2d63f3bd8cc9dfd7fe82601c96dd7702ae4739ee
Vadym Hevlich <vege1wgw@gmail.com>
2017-03-20 02:26:28 +0200

    procfile upd

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn portuss.wsgi --log-file -
--- portuss/settings.py
+++ portuss/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for gettingstarted project, on Heroku. For more info, see:
+Django settings for portuss project, on Heroku. For more info, see:
 https://github.com/heroku/heroku-django-template
 
 For more information on this file, see
