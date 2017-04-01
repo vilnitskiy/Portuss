ac3e3971973fa5d62cf667302d70ba71600126db
Vadym Hevlich <vege1wgw@gmail.com>
2017-04-02 02:27:14 +0300

    fix

--- static/js/payments_dropin.js
+++ static/js/payments_dropin.js
@@ -1,22 +1,18 @@
 $(document).ready(function() {
-
-  
-
-        function braintreeSetup() {
-            // Here you tell Braintree to add the drop-in to your division above
-            braintree.setup(braintree_client_token, "dropin", {
-                container: "braintree-dropin"
-                ,onError: function (obj) {
-                    // Errors will be added to the html code
-                    $('[type=submit]').prop('disabled', false);
-                    $('.braintree-notifications').html('<p class="alert alert-danger">' + obj.message + '</p>');
-                }
-            });
-        }
-        braintreeSetup();
-        $('form').submit(function () {
-            $('[type=submit]').prop('disabled', true);
-            $('.braintree-notifications').html('');
+    function braintreeSetup() {
+        // Here you tell Braintree to add the drop-in to your division above
+        braintree.setup(braintree_client_token, "dropin", {
+            container: "braintree-dropin"
+            ,onError: function (obj) {
+                // Errors will be added to the html code
+                $('[type=submit]').prop('disabled', false);
+                $('.braintree-notifications').html('<p class="alert alert-danger">' + obj.message + '</p>');
+            }
         });
-    
+    }
+    braintreeSetup();
+    $('form').submit(function () {
+        $('[type=submit]').prop('disabled', true);
+        $('.braintree-notifications').html('');
+    });
 });
