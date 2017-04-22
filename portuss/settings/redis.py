9039406b9dab1f161ed8abf8025b474371e42584
Vadym Hevlich <vege1wgw@gmail.com>
2017-04-22 20:19:30 +0300

    redis settings changed

--- portuss/settings/redis.py
+++ portuss/settings/redis.py
@@ -1,6 +1,5 @@
 from .base import *
 
-redis_host = os.environ.get('REDIS_HOST', 'localhost')
 
 # Channel layer definitions
 # http://channels.readthedocs.org/en/latest/deploying.html#setting-up-a-channel-backend
@@ -9,7 +8,7 @@ CHANNEL_LAYERS = {
         # This example app uses the Redis channel layer implementation asgi_redis
         "BACKEND": "asgi_redis.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(redis_host, 6379)],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
         "ROUTING": "portuss.routing.channel_routing",
     },
